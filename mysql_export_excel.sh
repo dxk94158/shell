3805f1075e9de0a8e0168b49e865f3e916d557e9
Frank <fengxuting@gmail.com>
2014-09-08 00:07:57 +0800

    mysql export excel'

--- mysql_export_excel.sh
+++ mysql_export_excel.sh
@@ -1,1 +1,13 @@
-mysql ¨Cdefault-character-set='gb2312' 
+#!/bin/bash
+# export table fitment to excel
+# author:frank
+# date:2014-09-08
+
+user=mbtiretool
+pass=e4fdf8ebba
+db=mbtiretool
+table=fitment
+dir=/backup/mbtiretool/
+file=fitment.xls
+
+mysql --default-character-set='gb2312' -u${user} -p${pass} -e "select * from ${db}.${table}" > ${dir}${file}
